4633078739844a299de0d3279503242fcbb8c9c6
NicholasStine <nickstine1450@gmail.com>
2020-07-31 19:15:34 -0700

    07/31/2020 - First commit / with matrix inversion and caching appearing to be functional. Unsure of program functionality under test conditions.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Nicholas Stine
+## 07/27/2020
+## Programming Assingment 2
 
-## Write a short comment describing this function
-
+## Generate the "special" matrix in which the value and result of inversion can be cached
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y){
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinv <- function(arg_inv) inv <<- arg_inv
+  getinv <- function() inv
+  list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
-
+## Return the cached result of inversion, or (if the result has not yet been calculated) then solve, cache and return
+## the newly inverted matrix
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  data <- x$get()
+  inv <- x$getinv()
+  if(!is.null(inv)){
+    message("getting cached data")
+    return(inv)
+  }
+  inv <- solve(data)
+  x$setinv(inv)
+  inv
 }
